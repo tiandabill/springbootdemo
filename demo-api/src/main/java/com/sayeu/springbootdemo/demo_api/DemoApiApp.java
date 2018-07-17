585b917ab8126ff024e1cf67c0db496184797fc3
Bill Liu <tiandabill@msn.com>
2018-07-17 14:42:41 +0200

    add springbootapplication into files

---
+++ demo-api/src/main/java/com/sayeu/springbootdemo/demo_api/DemoApiApp.java
@@ -0,0 +1,15 @@
+package com.sayeu.springbootdemo.demo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(DemoApiApp.class, args);
+	}
+
+}
